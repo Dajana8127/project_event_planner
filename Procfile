6af8152d19898a9b367ca3665dd4062952d8d987
Dajana8127 <dajana.stankovic94@gmail.com>
2020-08-06 12:54:36 -0400

    deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn project_event_planner.wsgi
+release: python manage.py migrate
--- project_event_planner/settings.py
+++ project_event_planner/settings.py
@@ -64,6 +64,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -148,6 +151,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Static files (CSS, JavaScript, Images)
